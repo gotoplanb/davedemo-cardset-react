289fc88b930922e662098a31f02afcc842bf1553
Dave Stanton <davestanton.us@gmail.com>
2017-01-25 14:57:21 -0500

    Use react-router <Link /> instead of anchors.

--- src/components/Database.js
+++ src/components/Database.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { Link } from 'react-router';
 
 class Database extends Component {
   render() {
     return (
       <div className="Database">
         <Header title="Database" />
+        <h2>Tables</h2>
+        <ul>
+          <li><Link to="/card">card</Link></li>
+          <li><Link to="/cardset">cardset</Link></li>
+        </ul>
       </div>
     );
   }
