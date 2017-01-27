d7a8de57057bf2585a1201b8b45437e278e7412d
Dave Stanton <davestanton.us@gmail.com>
2017-01-27 11:25:51 -0500

    Add Lightning Design System.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-
+import './styles/salesforce-lightning-design-system.min.css'
 import './index.css';
+
 import Database from './components/Database';
 import Table from './components/Table';
 import Record from './components/Record';
